79edfa5af221d3d6ab7c24c830663f67708a6b9b
Mizuki Morohoshi <8080suisho@gmail.com>
2020-09-26 01:46:48 +0900

    課題　First Commit

---
+++ Pods/Target Support Files/Pods-ProductHomework/Pods-ProductHomework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProductHomework : NSObject
+@end
+@implementation PodsDummy_Pods_ProductHomework
+@end
--- ProductHomework/ViewController.swift
+++ ProductHomework/ViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var nameTextField: UITextField!
+    
+    @IBAction func didTapLoginButton(){
+        self.performSegue(withIdentifier: "toChat", sender: nil)
+    }
+    
+    func showErrorIfNeeded(_ errorOrNil: Error?) {
+        // エラーがなければ何もしません
+        guard errorOrNil != nil else { return }
+        
+        let message = "エラーが起きました" // ここは後述しますが、とりあえず固定文字列
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
